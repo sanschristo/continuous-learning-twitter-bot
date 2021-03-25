1df573f6a20929af4025c6754980ff31b0b57f6d
chrissa <chris.sams@dat.com>
2021-03-25 08:37:34 -0700

    Adding node-cron, removing cron workflow

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { startRetweet, startTweetRedditPosts } from './controller/controller';
+import * as cron from 'node-cron';
 
-startRetweet();
-startTweetRedditPosts();
+const cronString = '0 * * * *';
+const main = () => {
+    startRetweet();
+    startTweetRedditPosts();
+};
+
+cron.schedule(cronString, main);
